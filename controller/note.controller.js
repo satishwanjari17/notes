555d0663b490dc0c8639509557bccfa1988c3bd9
satishwanjari17 <satishwanjari17@gmal.com>
2020-01-17 15:30:44 +0530

    all files

--- controller/note.controller.js
+++ controller/note.controller.js
@@ -30,7 +30,7 @@ exports.create = (req, res) => {
 
 // Retrieve and return all notes from the database.
 exports.findAll = (req, res) => {
-    Note.find()
+    Note.find({userId:req.params.userId})
     .then(notes => {
         res.send(notes);
     }).catch(err => {
